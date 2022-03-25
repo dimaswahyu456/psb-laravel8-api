171b25284bd50f2f5b672ef024874fc487605887
Dimas <dimaswahyu456@gmail.com>
2022-03-25 21:36:08 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('/siswa', App\Http\Controllers\Api\SiswaController::class);
