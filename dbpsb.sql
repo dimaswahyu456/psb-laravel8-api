7e0d506f742c2de4d846f1f28144203f7a1968fd
Dimas <dimaswahyu456@gmail.com>
2022-04-02 14:10:41 +0700

    Update Database

--- dbpsb.sql
+++ dbpsb.sql
@@ -1,31 +1,112 @@
 /*
- Navicat Premium Data Transfer
+SQLyog Ultimate v10.42 
+MySQL - 5.5.5-10.4.21-MariaDB : Database - db_psb
+*********************************************************************
+*/
 
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 100417
- Source Host           : localhost:3306
- Source Schema         : dbpsb
+/*!40101 SET NAMES utf8 */;
 
- Target Server Type    : MySQL
- Target Server Version : 100417
- File Encoding         : 65001
+/*!40101 SET SQL_MODE=''*/;
 
- Date: 25/03/2022 20:29:23
-*/
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE `db_psb`;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+/*Table structure for table `kecamatan` */
 
--- ----------------------------
--- Table structure for siswa
--- ----------------------------
-DROP TABLE IF EXISTS `siswa`;
-CREATE TABLE `siswa`  (
+DROP TABLE IF EXISTS `kecamatan`;
+
+CREATE TABLE `kecamatan` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nama_siswa` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `alamat_siswa` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  `kecamatan` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-SET FOREIGN_KEY_CHECKS = 1;
+/*Data for the table `kecamatan` */
+
+/*Table structure for table `prestasi` */
+
+DROP TABLE IF EXISTS `prestasi`;
+
+CREATE TABLE `prestasi` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `prestasi` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `prestasi` */
+
+/*Table structure for table `sekolah` */
+
+DROP TABLE IF EXISTS `sekolah`;
+
+CREATE TABLE `sekolah` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sekolah` varchar(255) DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `id_kec` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_kec` (`id_kec`),
+  CONSTRAINT `sekolah_ibfk_1` FOREIGN KEY (`id_kec`) REFERENCES `kecamatan` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sekolah` */
+
+/*Table structure for table `siswas` */
+
+DROP TABLE IF EXISTS `siswas`;
+
+CREATE TABLE `siswas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nisn` varchar(18) DEFAULT NULL,
+  `nama_siswa` varchar(255) DEFAULT NULL,
+  `alamat_siswa` text DEFAULT NULL,
+  `jenis_kelamin` varchar(9) DEFAULT NULL,
+  `agama` varchar(10) DEFAULT NULL,
+  `tempat_lahir` varchar(25) DEFAULT NULL,
+  `tanggal_lahir` date DEFAULT NULL,
+  `id_sekolah` int(11) DEFAULT NULL,
+  `id_prestasi` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `id_sekolah` (`id_sekolah`),
+  KEY `id_prestasi` (`id_prestasi`),
+  CONSTRAINT `siswas_ibfk_1` FOREIGN KEY (`id_sekolah`) REFERENCES `sekolah` (`id`),
+  CONSTRAINT `siswas_ibfk_2` FOREIGN KEY (`id_prestasi`) REFERENCES `prestasi` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `siswas` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(8) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `users_role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `users` */
+
+/*Table structure for table `users_role` */
+
+DROP TABLE IF EXISTS `users_role`;
+
+CREATE TABLE `users_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `users_role` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
